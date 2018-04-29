7d11b86de5204da64beb134dce5c1e73248f72d6
lelam <lminhlam@gmail.com>
2018-04-29 09:50:27 +0700

    Fix show value when user on Edit mode of Module

--- src/Installs/resources/views/la/modules/field_edit.blade.php
+++ src/Installs/resources/views/la/modules/field_edit.blade.php
@@ -1,151 +1,146 @@
 @extends("la.layouts.app")
-
-@section("contentheader_title", "Edit Field: ".$field->label)
-@section("contentheader_description", "from ".$module->model." module")
-@section("section", "Module ".$module->name)
-@section("section_url", url(config('laraadmin.adminRoute') . '/modules/'.$module->id))
-@section("sub_section", "Edit Field")
-
-@section("htmlheader_title", "Field Edit : ".$field->label)
-
+@section("contentheader_title")
+	<a href="{{ url(config('laraadmin.adminRoute') . '/modules') }}">{{trans('/modules/Modules.view.module_name')}}</a> :
+@endsection
+@section("contentheader_description", $module->model)
+@section("section", trans('/modules/Modules.view.model'))
+@section("section_url", url(config('laraadmin.adminRoute') . '/modules'.$module->id))
+@section("sub_section", trans('view.edit'))
+@section("htmlheader_title", trans('view.edit') .' : '.$module->model)
 @section("main-content")
-<div class="box">
-	<div class="box-header">
+@if($errors->any())
+<ul class="alert alert-danger">
+	@foreach($errors->all() as $error)
+		<li>{{ $error }}</li>
+	@endforeach
+</ul>
+@endif
+@section ("form_content")
+<div class="row">
+	<div class="col-md-8 col-md-offset-2">
+	{{ Form::hidden("module_id", $module->id) }}
+					
+		<div class="form-group">
+			<label for="label">{{trans('/modules/Modules.database.field_name')}} :</label>
+			{{ Form::text("label", $field->label, ['class'=>'form-control', 'placeholder'=>'Field Label', 'data-rule-minlength' => 2, 'data-rule-maxlength'=>30, 'required' => 'required']) }}
+		</div>
 		
-	</div>
-	<div class="box-body">
-		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
-				{!! Form::model($field, ['route' => [config('laraadmin.adminRoute') . '.module_fields.update', $field->id ], 'method'=>'PUT', 'id' => 'field-edit-form']) !!}
-					{{ Form::hidden("module_id", $module->id) }}
-					
-					<div class="form-group">
-						<label for="label">Field Label :</label>
-						{{ Form::text("label", null, ['class'=>'form-control', 'placeholder'=>'Field Label', 'data-rule-minlength' => 2, 'data-rule-maxlength'=>30, 'required' => 'required']) }}
-					</div>
-					
-					<div class="form-group">
-						<label for="colname">Column Name :</label>
-						{{ Form::text("colname", null, ['class'=>'form-control', 'placeholder'=>'Column Name (lowercase)', 'data-rule-minlength' => 2, 'data-rule-maxlength'=>100, 'data-rule-banned-words' => 'true', 'required' => 'required']) }}
-					</div>
-					
-					<div class="form-group">
-						<label for="field_type">UI Type:</label>
-						{{ Form::select("field_type", $ftypes, null, ['class'=>'form-control', 'required' => 'required']) }}
-					</div>
-					
-					<div id="unique_val">
-						<div class="form-group">
-							<label for="unique">Unique:</label>
-							{{ Form::checkbox("unique", "unique") }}
-							<div class="Switch Round Off" style="vertical-align:top;margin-left:10px;"><div class="Toggle"></div></div>
-						</div>
-					</div>
-
-					<div class="form-group">
-						<label for="defaultvalue">Default Value :</label>
-						{{ Form::text("defaultvalue", null, ['class'=>'form-control', 'placeholder'=>'Default Value']) }}
-					</div>
-					<div class="form-group">
-						<label for="is_copy">Is Copy:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-						{{ Form::checkbox("is_copy", "is_copy") }}
-							<div class="Switch Round Off" style="vertical-align:top;margin-left:10px;"><div class="Toggle"></div></div>
-					</div>
-					<div class="form-group">
-						<label for="is_translate">Is Translate :</label>
-						{{ Form::checkbox("is_translate", "is_translate") }}
-							<div class="Switch Round Off" style="vertical-align:top;margin-left:10px;"><div class="Toggle"></div></div>
-					</div>
-
-					<div class="form-group">
-						<label for="is_same">Is Same :</label>
-						{{ Form::checkbox("is_same", "is_same") }}
-							<div class="Switch Round Off" style="vertical-align:top;margin-left:10px;"><div class="Toggle"></div></div>
-					</div>
-					
-					<div id="length_div">
-						<div class="form-group">
-							<label for="minlength">Minimum :</label>
-							{{ Form::number("minlength", null, ['class'=>'form-control', 'placeholder'=>'Default Value']) }}
-						</div>
-						
-						<div class="form-group">
-							<label for="maxlength">Maximum :</label>
-							{{ Form::number("maxlength", null, ['class'=>'form-control', 'placeholder'=>'Default Value']) }}
-						</div>
-					</div>
-					
-					<div class="form-group">
-						<label for="required">Required:</label>
-						{{ Form::checkbox("required", "required") }}
-						<div class="Switch Round Off" style="vertical-align:top;margin-left:10px;"><div class="Toggle"></div></div>
-					</div>
-					
-					<div class="form-group values">
-						<label for="popup_vals">Values :</label>
-						<?php
-						$default_val = "";
-						$popup_value_type_table = false;
-						$popup_value_type_list = false;
-						if(starts_with($field->popup_vals, "@")) {
-							$popup_value_type_table = true;
-							$default_val = str_replace("@", "", $field->popup_vals);
-						} else if(starts_with($field->popup_vals, "[")) {
-							$popup_value_type_list = true;
-							$default_val = json_decode($field->popup_vals);
-						}
-						?>
-						<div class="radio" style="margin-bottom:20px;">
-							<label>{{ Form::radio("popup_value_type", "table", $popup_value_type_table) }} From Table</label>
-							<label>{{ Form::radio("popup_value_type", "list", $popup_value_type_list) }} From List</label>
-						</div>
-						{{ Form::select("popup_vals_table", $tables, $default_val, ['class'=>'form-control', 'rel' => '']) }}
-						
-						<select class="form-control popup_vals_list" rel="taginput" multiple="1" data-placeholder="Add Multiple values (Press Enter to add)" name="popup_vals_list[]">
-							@if(is_array($default_val))
-								@foreach ($default_val as $value)
-									<option selected>{{ $value }}</option>
-								@endforeach
-							@endif
-						</select>
-
-						<div class="form-group" style="margin-top:20px;">
-							<label for="defaultvalue">Filter Expressions :</label>
-							{{ Form::text("filter_expressions", null, ['class'=>'form-control', 'placeholder'=>'Filter Expressions']) }}
-						</div>
-						
-						<?php
-						// print_r($tables);
-						?>
-					</div>
-					
-                    <br>
-					<div class="form-group">
-						{!! Form::submit( 'Update', ['class'=>'btn btn-success']) !!} <button class="btn btn-default pull-right"><a href="{{ url(config('laraadmin.adminRoute') . '/modules/'.$module->id) }}">Cancel</a></button>
-					</div>
-				{!! Form::close() !!}
-				
-				@if($errors->any())
-				<ul class="alert alert-danger">
-					@foreach($errors->all() as $error)
-						<li>{{ $error }}</li>
-					@endforeach
-				</ul>
-				@endif
+		<div class="form-group">
+			<label for="colname">{{trans('/modules/Modules.database.field_name')}} :</label>
+			{{ Form::text("colname", $field->colname, ['class'=>'form-control', 'placeholder'=>'Column Name (lowercase)', 'data-rule-minlength' => 2, 'data-rule-maxlength'=>100, 'data-rule-banned-words' => 'true', 'required' => 'required']) }}
+		</div>
+		
+		<div class="form-group">
+			<label for="field_type">{{trans('/modules/Modules.database.field_ui_type')}}:</label>
+			{{ Form::select("field_type", $ftypes, $field->field_type, ['class'=>'form-control', 'required' => 'required']) }}
+		</div>
+		
+		<div id="unique_val">
+			<div class="form-group">
+				<label for="unique">{{trans('/modules/Modules.database.field_unique')}}:</label>
+				{{ Form::checkbox("unique", $field->unique) }}
+				<div class="Switch Round Off" style="vertical-align:top;margin-left:10px;"><div class="Toggle"></div></div>
 			</div>
 		</div>
+
+		<div class="form-group">
+			<label for="defaultvalue">{{trans('/modules/Modules.database.field_default_value')}} :</label>
+			{{ Form::text("defaultvalue", $field->defaultvalue, ['class'=>'form-control', 'placeholder'=>'Default Value']) }}
+		</div>
+		<div class="form-group">
+			<label for="is_copy">{{trans('/modules/Modules.database.field_is_copy')}}:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+			{{ Form::checkbox("is_copy", $field->is_copy) }}
+				<div class="Switch Round Off" style="vertical-align:top;margin-left:10px;"><div class="Toggle"></div></div>
+		</div>
+		<div class="form-group">
+			<label for="is_translate">{{trans('/modules/Modules.database.field_is_translate')}} :</label>
+			{{ Form::checkbox("is_translate", $field->is_translate) }}
+				<div class="Switch Round Off" style="vertical-align:top;margin-left:10px;"><div class="Toggle"></div></div>
+		</div>
+
+		<div class="form-group">
+			<label for="is_same">{{trans('/modules/Modules.database.field_is_same')}} :</label>
+			{{ Form::checkbox("is_same", $field->is_same) }}
+				<div class="Switch Round Off" style="vertical-align:top;margin-left:10px;"><div class="Toggle"></div></div>
+		</div>
+		
+		<div id="length_div">
+			<div class="form-group">
+				<label for="minlength">{{trans('/modules/Modules.database.field_minimum')}} :</label>
+				{{ Form::number("minlength", $field->minlength, ['class'=>'form-control', 'placeholder'=>'Default Value']) }}
+			</div>
+			
+			<div class="form-group">
+				<label for="maxlength">{{trans('/modules/Modules.database.field_maximum')}} :</label>
+				{{ Form::number("maxlength", $field->maxlength, ['class'=>'form-control', 'placeholder'=>'Default Value']) }}
+			</div>
+		</div>
+		
+		<div class="form-group">
+			<label for="required">{{trans('/modules/Modules.database.field_required')}}:</label>
+			{{ Form::checkbox("required", $field->required) }}
+			<div class="Switch Round Off" style="vertical-align:top;margin-left:10px;"><div class="Toggle"></div></div>
+		</div>
+		
+		<div class="form-group values">
+			<label for="popup_vals">{{trans('/modules/Modules.database.field_values')}} :</label>
+			<?php
+			$default_val = "";
+			$popup_value_type_table = false;
+			$popup_value_type_list = false;
+			if(starts_with($field->popup_vals, "@")) {
+				$popup_value_type_table = true;
+				$default_val = str_replace("@", "", $field->popup_vals);
+			} else if(starts_with($field->popup_vals, "[")) {
+				$popup_value_type_list = true;
+				$default_val = json_decode($field->popup_vals);
+			}
+			?>
+			<div class="radio" style="margin-bottom:20px;">
+				<label>{{ Form::radio("popup_value_type", "table", $popup_value_type_table) }} {{trans('/modules/Modules.database.field_form_table')}}</label>
+				<label>{{ Form::radio("popup_value_type", "list", $popup_value_type_list) }} {{trans('/modules/Modules.database.field_form_list')}}</label>
+			</div>
+			{{ Form::select("popup_vals_table", $tables, $default_val, ['class'=>'form-control', 'rel' => '']) }}
+			
+			<select class="form-control popup_vals_list" rel="taginput" multiple="1" data-placeholder="Add Multiple values (Press Enter to add)" name="popup_vals_list[]">
+				@if(is_array($default_val))
+					@foreach ($default_val as $value)
+						<option selected>{{ $value }}</option>
+					@endforeach
+				@endif
+			</select>
+
+			<div class="form-group" style="margin-top:20px;">
+				<label for="defaultvalue">{{trans('/modules/Modules.database.field_filter_expressions')}} :</label>
+				{{ Form::text("filter_expressions", $field->filter_expressions, ['class'=>'form-control', 'placeholder'=>'Filter Expressions']) }}
+			</div>
+			
+			<?php
+			// print_r($tables);
+			?>
+		</div>
+		
+		<br>
+		<div class="form-group">
+			{!! Form::submit( 'Update', ['class'=>'btn btn-success']) !!} <button class="btn btn-default pull-right"><a href="{{ url(config('laraadmin.adminRoute') . '/modules/'.$module->id) }}">Cancel</a></button>
+		</div>
+
+			
 	</div>
-</div>
-
 @endsection
-
+@include('la.layouts.partials.default_edit',[
+	'form_model' => $module,
+	'form_module_name' => 'module_fields',
+	'form_key_value' => $module->id,
+	'form_action_id_tpl' => 'field-edit-form'
+])
+@endsection
 @push('scripts')
 <script>
 $(function () {
 	$("select.popup_vals_list").show();
 	$("select.popup_vals_list").next().show();
 	$("select[name='popup_vals_table']").hide();
-	
 	function showValuesSection() {
 		var ft_val = $("select[name='field_type']").val();
 		if(ft_val == 7 || ft_val == 15 || ft_val == 18 || ft_val == 20) {
@@ -153,23 +148,19 @@ $(function () {
 		} else {
 			$(".form-group.values").hide();
 		}
-		
 		$('#length_div').removeClass("hide");
 		if(ft_val == 2 || ft_val == 4 || ft_val == 5 || ft_val == 7 || ft_val == 9 || ft_val == 11 || ft_val == 12 || ft_val == 15 || ft_val == 18 || ft_val == 21 || ft_val == 24 ) {
 			$('#length_div').addClass("hide");
 		}
-		
 		$('#unique_val').removeClass("hide");
 		if(ft_val == 1 || ft_val == 2 || ft_val == 3 || ft_val == 7 || ft_val == 9 || ft_val == 11 || ft_val == 12 || ft_val == 15 || ft_val == 18 || ft_val == 20 || ft_val == 21 || ft_val == 24 ) {
 			$('#unique_val').addClass("hide");
 		}
 	}
-
 	$("select[name='field_type']").on("change", function() {
 		showValuesSection();
 	});
 	showValuesSection();
-
 	function showValuesTypes() {
 		console.log($("input[name='popup_value_type']:checked").val());
 		if($("input[name='popup_value_type']:checked").val() == "list") {
@@ -182,18 +173,14 @@ $(function () {
 			$("select.popup_vals_list").next().hide();
 		}
 	}
-	
 	$("input[name='popup_value_type']").on("change", function() {
 		showValuesTypes();
 	});
 	showValuesTypes();
-
 	$.validator.addMethod("data-rule-banned-words", function(value) {
 		return $.inArray(value, ['files']) == -1;
 	}, "Column name not allowed.");
-
 	$("#field-edit-form").validate({
-		
 	});
 });
 </script>
